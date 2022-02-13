db79e54ccf698c792464cb9be51c548076280ec9
fabiobiondi <info@fabiobiondi.com>
2022-02-13 03:33:21 +0100

    add key

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 function Contacts() {
+  const label = process.env.NEXT_PUBLIC_MY_CUSTOM_KEY;
   return (
     <>
-      <h1 className="title">Contacts</h1>
+      <h1 className="title">Contacts {label}</h1>
     </>
   )
 }
