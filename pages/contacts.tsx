93b5ffb4158933cb01bb8f27358e9474c7e324e5
fabiobiondi <info@fabiobiondi.com>
2022-02-13 03:44:45 +0100

    add env key

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 function Contacts() {
-  const label = process.env.NEXT_PUBLIC_MY_CUSTOM_KEY;
   return (
     <>
-      <h1 className="title">Contacts {label}</h1>
+      <h1 className="title">Contacts { process.env.NEXT_PUBLIC_CUSTOM_KEY}</h1>
     </>
   )
 }
