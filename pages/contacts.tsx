a574b6ea0796ad593f3bdceaa5e8b0ff5afcaf4b
fabiobiondi <info@fabiobiondi.com>
2022-02-14 21:30:27 +0100

    removed env variables

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Contacts() {
   return (
     <>
-      <h1 className="title">Contacts { process.env.NEXT_PUBLIC_CUSTOM_KEY}</h1>
+      <h1 className="title">Contacts</h1>
     </>
   )
 }
